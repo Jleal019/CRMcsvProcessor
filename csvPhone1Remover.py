3fdfc7ea25b1bf99a73b2005501217cce920abde
Jleal019 <jleal019@fiu.edu>
2019-10-03 17:43:29 -0400

    csvProcessor will now delete what is needed

--- csvPhone1Remover.py
+++ csvPhone1Remover.py
@@ -1,5 +1,6 @@
 import tkinter
 import pandas as pd
+import numpy as np
 
 fileName = 'export.csv'
 
@@ -10,12 +11,8 @@ def removeDupe():
         data = pd.read_csv(fileName, encoding="utf-8", dtype=object)
 
         # In 'subset' write all the fields you want to dedup with
-        data.drop_duplicates(subset=['Phone 1'], keep='first')
 
-        print(data.drop_duplicates(subset=['Phone 1'], keep='first'))
-
-        # saves new CSV with removed dupes
-        # data.to_csv(r'unDuped.csv')
+        data.drop_duplicates(subset='Phone 1.1', keep='last').to_csv('unDuped.csv')
 
         print("CSV generated.")
 
