048608f071f7945755f6e3ffd3cabdda3f4d50f1
Jleal019 <jleal019@fiu.edu>
2019-10-03 10:53:11 -0400

    Intialization commit

--- csvProcessor.py
+++ csvProcessor.py
@@ -3,14 +3,7 @@ import csv
 fileName = 'export.csv'
 
 
-def read_csv(input_file):
-    with read_funding_(input_file, newline='') as file:
-        csvReader = csv.reader(file, delimiter=' ', quotechar='|')
-        for row in csvReader:
-            yield row
-
-if __name__ == "__main__":
-    for indx, row in enumerate(read_csv(fileName)):
-        if indx > 10: break
-        print()
-
+with open(input_file, newline='') as file:
+    csvReader = csv.reader(file, delimiter=',')
+    for row in csvReader:
+        print(row)
