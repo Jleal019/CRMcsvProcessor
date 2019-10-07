d090a132a108bfbfb901dfea42e917586b1938c9
Jleal019 <jleal019@fiu.edu>
2019-10-07 11:45:44 -0400

    current functions seem to work correctly

--- csvPhone1Remover.py
+++ csvPhone1Remover.py
@@ -8,16 +8,21 @@ fileName = 'export.csv'
 def removeDupe():
 
     # loads CSV into file
-    data = pd.read_csv(fileName, encoding="utf-8", dtype=object)
+    data = pd.read_csv(fileName, delimiter=",", encoding="utf-8", dtype=object)
+    print(data, '\n -------------------Unprocessed data above---------------------')
 
     # See output of program
+    # In 'subset' write all the fields you want to dedup with
     # print(data.drop_duplicates(subset='Phone 1.1', keep="last"))
+
     # data.drop_duplicates(subset='Phone 1.1', keep="last").to_csv('unDuped-by-Phone.csv')
 
-    # In 'subset' write all the fields you want to dedup with
-    # data[['Name', 'Phone 1.1s']].duplicated(subset='Phone 1.1', keep='last').to_csv('DupForManualRemoval.csv')
+    # Prints specific column values
+    print(data[["Name", "Phone 1.1"]].drop_duplicates(subset='Phone 1.1', keep='last'))
+    # [data[["Name", "Phone 1.1"]].drop_duplicates(subset='Phone 1.1', keep='last').to_csv('SpecificColumns.csv')
 
-    # to_csv by itself just gets rid of apostrophes in empty columns
+
+    # to_csv by itself just gets rid of quotations in empty columns
     # data.to_csv('unDuped.csv')
     print("CSV generated.")
 
