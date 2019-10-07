050c1fd98c65e581f77e7616000011b2df7d10ac
Jleal019 <jleal019@fiu.edu>
2019-10-07 10:45:58 -0400

    May still be bugged
    Backup for all current functions

--- csvPhone1Remover.py
+++ csvPhone1Remover.py
@@ -6,19 +6,20 @@ fileName = 'export.csv'
 
 
 def removeDupe():
-    try:
-        # loads CSV into file
-        data = pd.read_csv(fileName, encoding="utf-8", dtype=object)
 
-        # In 'subset' write all the fields you want to dedup with
+    # loads CSV into file
+    data = pd.read_csv(fileName, encoding="utf-8", dtype=object)
 
-        data.drop_duplicates(subset='Phone 1.1', keep='last').to_csv('unDuped.csv')
+    # See output of program
+    # print(data.drop_duplicates(subset='Phone 1.1', keep="last"))
+    # data.drop_duplicates(subset='Phone 1.1', keep="last").to_csv('unDuped-by-Phone.csv')
 
-        print("CSV generated.")
+    # In 'subset' write all the fields you want to dedup with
+    # data[['Name', 'Phone 1.1s']].duplicated(subset='Phone 1.1', keep='last').to_csv('DupForManualRemoval.csv')
 
-    except KeyError:
-        print("It seems you have entered some values incorrectly. Please try again.")
-        removeDupe()
+    # to_csv by itself just gets rid of apostrophes in empty columns
+    # data.to_csv('unDuped.csv')
+    print("CSV generated.")
 
 
 removeDupe()
