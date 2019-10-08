494c4957655286f6b64c6753b4f1cfa371599cc4
Jleal019 <jleal019@fiu.edu>
2019-10-08 15:13:29 -0400

    adding more functionality and broke a bunch of other stuff. Still need to add a way to remove leading ones.

--- csvPhone1Remover.py
+++ csvPhone1Remover.py
@@ -1,24 +1,43 @@
-import tkinter
-import csv
-import pandas
-import numpy
+import pandas, numpy, csv, tkinter, re
+
+'''
+-Fix last name issue
+
+-Deleting columns is faster in Excel/Calc
+'''
 
 fileName = 'export.csv'
 
-'''Fix last name issue
-'''
+
+# creates CSV dataframe using pandas
+data = pandas.read_csv(fileName, delimiter=",", encoding="utf-8", dtype=object)
+
+# removes hello@gmail.com use from the Email column
+data = data.replace(to_replace='hello@gmail.com', value='')
+
+# removes leading 1's and +1's in Phone 1.1 and Phone 1 field
+
+
+
+print(data[['First Name', 'Email', 'Phone 1', 'Phone 1.1']])
 
 
 def removeDupe():
 
     # loads CSV into file
-    data = pandas.read_csv(fileName, delimiter=",", encoding="utf-8", dtype=object)
     print(data, '\n -------------------Unprocessed data above---------------------')
 
-    # --- See output of program ---
-    # In 'subset', write all the fields you want to dedup with
+    # --- See output of program without changes ---
     # print(data.drop_duplicates(subset='Phone 1.1', keep="last"))
 
+    # --- Remove trailing 1's ---
+
+
+
+
+    # In 'subset', write all the fields you want to deduplicate with
+    # A new file will be created called 'unDuped-by-Phone.csv'
+    # This is the file without duplicates
     # data.drop_duplicates(subset='Phone 1.1', keep="last").to_csv('unDuped-by-Phone.csv')
 
     # Prints specific column values
@@ -26,18 +45,21 @@ def removeDupe():
     data[["Name", "Phone 1.1"]].drop_duplicates(subset='Phone 1.1', keep='last').to_csv('Specific-Columns.csv')
 
     # to_csv by itself just gets rid of quotations in empty columns
-    # data.to_csv('unDuped.csv')
+    # data.to_csv('unQuoted.csv')
     print("CSV generated.")
 
-    findDiff(fileName, 'test.csv')
+    # findDiff(fileName, 'test.csv')
 
 
 def findDiff(ref, diff):
     print("---------------- Diff Data ---------------------------")
 
-    data = pandas.read_csv(ref, delimiter=',', encoding="utf-8", dtype=object)
+    data1 = pandas.read_csv(fileName, delimiter=',', encoding="utf-8", dtype=object)
     data2 = pandas.read_csv(diff, delimiter=',', encoding="utf-8", dtype=object)
-    catData = pandas.concat([data, data2], axis=1)
+
+    catData = pandas.concat([data1, data2], axis=1)
+
+    print(catData.drop_duplicates(subset='Phone 1.1'))
 
     print(catData[["Name", "Phone 1.1"]])
 
