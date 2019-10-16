5e0cae885e2370e31c8aec5966c72ec33ea53bb0
Jleal019 <jleal019@fiu.edu>
2019-10-16 12:58:36 -0400

    Script is finished

--- NameFix.py
+++ NameFix.py
@@ -9,7 +9,7 @@ def nameFix():
         with open(fileName, 'r', encoding='utf-8') as csvFile:
             csvReader = csv.DictReader(csvFile, delimiter=',')
             # opens and creates file to write to.
-            csvWriter = csv.DictWriter(open('FixedNames.csv', 'w+', newline=''), fieldnames=["Id", "Name", "First Name", "Last Name", "Phone 1.1", "Email"])
+            csvWriter = csv.DictWriter(open('FixedNames.csv', 'w+', newline='',encoding='utf-8'), fieldnames=["Id", "Name", "First Name", "Last Name", "Phone 1.1", "Email"])
             csvWriter.writeheader()
             nuLines = {}
 
@@ -27,41 +27,44 @@ def nameFix():
                 # if last_Name is blank, Name contains more than 2 values, and firstName contains more
                 # than 1
                 if last_Name == '' and len(Name.split()) >= 2 and len(first_Name.split()) > 1:
+                    print("Pre-Case 1", nuLines)
                     # print("LENGTH", len(Name.split()))
                     Bi = first_Name.split()
                     first_Name = Bi[0]
                     last_Name = Bi[1:]
                     # join needed so that last_Name list is converted to string
-                    last_Name = ''.join(last_Name)
+                    last_Name = ' '.join(last_Name)
 
                     Name = first_Name, last_Name
                     Name = ' '.join(Name)
                     # nuLines = [Id, Name, first_Name, last_Name, phone1, email]
                     nuLines.update({'Id': Id, 'Name': Name, 'First Name': first_Name, 'Last Name': last_Name, 'Phone 1.1': phone1, 'Email': email})
-                    print(nuLines)
+                    print("Case 1 ", nuLines)
                     # print(Id, ', Name ', *Name, ', fName', first_Name, ', lName', *last_Name, ', phone', phone1, 'Email ', email)
 
                     csvWriter.writerow(nuLines)
 
                 # elif last_Name is in first_Name
-                elif last_Name in first_Name:
+                elif len(last_Name.split()) >= 1 and last_Name in first_Name:
+                    print("Pre-Case 2", nuLines)
                     # creates list
                     # removes last name from first_Name array and reassigns to first_Name
                     fName = first_Name.split()
-                    fName.remove(last_Name)
-                    first_Name = fName
-
+                    last_Name = last_Name.split()
+                    first_Name = fName[0]
                     first_Name = ''.join(first_Name)
+                    last_Name = ' '.join(last_Name)
 
-                    Name = first_Name
+                    Name = first_Name, last_Name
+                    Name = ' '.join(Name)
                     nuLines.update({'Id': Id, 'Name': Name, 'First Name': first_Name, 'Last Name': last_Name, 'Phone 1.1': phone1, 'Email': email})
-                    print(nuLines)
-
+                    print("Case 2", nuLines)
                     csvWriter.writerow(nuLines)
 
                 # prints everything else to csv (can probably be left commented out
                 # else:
                 #     nuLines.update({'Id': Id, 'Name': Name, 'First Name': first_Name, 'Last Name': last_Name, 'Phone 1.1': phone1, 'Email': email})
+                #     print("Default case", nuLines)
                 #     csvWriter.writerow(nuLines)
 
     finally:
